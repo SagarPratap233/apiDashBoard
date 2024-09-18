2cf8bca6eb937cc4b08506bdec05e86a51842946
SagarPratap233 <sagarpratapsp@gmail.com>
2024-09-19 02:06:56 +0530

    after apiHealthCheck function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
 import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+import {checkAPIHealth} from './healthChecks/apiHealthCheck';
+
+
+
+dotenv.config();
 
 const app = express();
-const  port = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response)=> {
-    res.send("Well");
+// app.use(express.json());
+app.get('/', async (req: Request, res: Response)=> {
+    res.json({"homepage": true});
 
 });
 
-app.listen(8000, ()=> {
-    console.log("Server is listening at 8000")
+app.get('/apiHealth', async (req: Request, res: Response) => {
+    const url = req.query.url as string;
+    if(!url)
+        {
+            res.status(500).json({"error": "Url is required"});
+        }
+
+    try{
+        const result = await checkAPIHealth(url);
+        res.status(200).json(result);
+    }
+    catch(error)
+    {
+        res.status(500).json({"error": "Internal server error"})
+    }
+  
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening at ${PORT}`)
 });
